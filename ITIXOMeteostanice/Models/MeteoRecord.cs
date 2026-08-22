53480c1bb123ebfdff64a9fcfaa50ec25a4c2829
Matej Pukancik <matejpukan@gmail.com>
2026-08-22 02:39:29 +0200

    Initial commit

---
+++ ITIXOMeteostanice/Models/MeteoRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteostanice.Models
+{
+    public class MeteoRecord
+    {
+        public int Id { get; set; }
+        public DateTime DownloadedAt { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? JSONData { get; set; }
+
+    }
+}

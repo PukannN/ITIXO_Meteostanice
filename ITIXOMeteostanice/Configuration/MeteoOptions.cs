109203cf9c0c3de1cb7bd344fdfd13aac4e95918
Matej Pukancik <matejpukan@gmail.com>
2026-08-23 12:06:07 +0200

    Change Interval Units

--- ITIXOMeteostanice/Configuration/MeteoOptions.cs
+++ ITIXOMeteostanice/Configuration/MeteoOptions.cs
@@ -7,6 +7,6 @@ namespace ITIXOMeteostanice.Configuration
     public class MeteoOptions
     {
         public string DownloadURL { get; set; } = String.Empty;
-        public int DownloadInterval { get; set; } = 1; // v hodinách
+        public int DownloadInterval { get; set; } = 1; // v minutách
     }
 }

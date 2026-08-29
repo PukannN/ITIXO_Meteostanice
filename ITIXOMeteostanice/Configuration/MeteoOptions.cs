766ed5fdc5e31476fe9d1eac4531a0d6a5d19090
Matěj Pukančík <144383789+PukannN@users.noreply.github.com>
2026-08-29 19:29:55 +0200

    Change DownloadInterval default to 60 minutes

--- ITIXOMeteostanice/Configuration/MeteoOptions.cs
+++ ITIXOMeteostanice/Configuration/MeteoOptions.cs
@@ -7,6 +7,6 @@ namespace ITIXOMeteostanice.Configuration
     public class MeteoOptions
     {
         public string DownloadURL { get; set; } = String.Empty;
-        public int DownloadInterval { get; set; } = 1; // v minutách
+        public int DownloadInterval { get; set; } = 60; // v minutách
     }
 }
